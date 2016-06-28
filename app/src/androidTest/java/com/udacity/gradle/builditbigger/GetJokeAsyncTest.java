438163bc49683726c4d9455f834690f9cd03c3ee
pilpin <sylvain.autran@gmail.com>
2016-06-29 00:05:02 +0200

    Added functionnal tests.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/GetJokeAsyncTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/GetJokeAsyncTest.java
@@ -1,5 +1,27 @@
 package com.udacity.gradle.builditbigger;
 
-public class GetJokeAsyncTest  {
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class GetJokeAsyncTest {
+    public String joke;
+
+    @Before
+    public void getJoke(){
+        GetJokeAsyncTask jokeAsyncTask = new GetJokeAsyncTask(null);
+        joke = jokeAsyncTask.doInBackground();
+    }
+
+    @Test
+    public void testDoInBackground() {
+        Assert.assertTrue("Joke is an empty string", joke.length() > 0);
+        Log.i("GCETest", joke);
+        Assert.assertFalse("Joke is not working properly : " + joke, joke.contains("failed to connect to"));
+    }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,5 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
